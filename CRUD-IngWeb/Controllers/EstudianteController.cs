f439735fe2ea26e5da777244dc05589757c3af02
JuliFlores21 <julian.flores.armijos@udla.edu.ec>
2024-09-27 15:14:29 -0500

    Se realizó el controlador y modelo

---
+++ CRUD-IngWeb/Controllers/EstudianteController.cs
@@ -0,0 +1,90 @@
+﻿using CRUD_IngWeb.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_IngWeb.Controllers
+{
+    public class EstudianteController : Controller
+    {
+        // GET: EstudianteController
+        public ActionResult Index()
+        {
+            var estudiantes = EstudianteRepository.GetEstudiantes();
+            return View(estudiantes);
+        }
+
+        // GET: EstudianteController/Details/5
+        public ActionResult Details(int id)
+        {
+            var estudiante = EstudianteRepository.GetEstudianteId(id);
+            if (estudiante == null)
+            {
+                return NotFound();
+            }
+            return View(estudiante);
+        }
+
+        // GET: EstudianteController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EstudianteController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Estudiante estudiante)
+        {
+            if(ModelState.IsValid)
+            {
+                EstudianteRepository.AgregarEstudiante(estudiante);
+                return RedirectToAction("Index");
+            }
+            return View(estudiante);
+        }
+
+        // GET: EstudianteController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var estudiante = EstudianteRepository.GetEstudianteId(id);
+            if(estudiante == null)
+            {
+                return NotFound();
+            }
+            return View(estudiante);
+        }
+
+        // POST: EstudianteController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Estudiante estudiante)
+        {
+            if (ModelState.IsValid)
+            {
+                EstudianteRepository.ActualizarEstudiante(estudiante);
+                return RedirectToAction("Index");
+            }
+            return View(estudiante);
+        }
+
+        // GET: EstudianteController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var estudiante = EstudianteRepository.GetEstudianteId(id);
+            if (estudiante == null)
+            {
+                return NotFound();
+            }
+            return View(estudiante);
+        }
+
+        // POST: EstudianteController/Delete/5
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            EstudianteRepository.EliminarEstudiante(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CRUD-IngWeb/Models/EstudianteRepository.cs
@@ -0,0 +1,49 @@
+﻿namespace CRUD_IngWeb.Models
+{
+    public class EstudianteRepository
+    {
+        private static List<Estudiante> estudiantes = new List<Estudiante>
+        {
+            new Estudiante{Id=1,Name="Julian",Email="julian23@gmail.com",Edad=15},
+            new Estudiante{Id=2,Name="Carlos",Email="carlos123@gmail.com",Edad=17}
+        };
+
+
+        public static List<Estudiante> GetEstudiantes() 
+        { 
+            return estudiantes;
+        }
+
+        public static Estudiante GetEstudianteId(int id) 
+        { 
+            return estudiantes.FirstOrDefault(e => e.Id==id);
+        }
+
+        public static void AgregarEstudiante(Estudiante estudiante)
+        {
+            estudiante.Id= estudiantes.Max(e => e.Id)+1;
+            estudiantes.Add(estudiante);
+        }
+
+        public static void ActualizarEstudiante(Estudiante estudiante)
+        {
+            var existeEstudiante = estudiantes.FirstOrDefault(e => e.Id == estudiante.Id);
+            if (existeEstudiante != null)
+            {
+                existeEstudiante.Name=estudiante.Name;
+                existeEstudiante.Email=estudiante.Email;
+                existeEstudiante.Edad=estudiante.Edad;
+            }
+        }
+
+        public static void EliminarEstudiante(int Id)
+        {
+            var estudiante = estudiantes.FirstOrDefault(e =>e.Id==Id);
+            if (estudiante != null)
+            {
+                estudiantes.Remove(estudiante);
+            }
+        }
+
+    }
+}
